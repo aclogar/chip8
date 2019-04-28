220d366c23369ffd63c5bb5c27eb2875ad8f01c1
Anthony Logar <coledude919@gmail.com>
2019-04-28 17:28:24 -0400

    Added a few more opcodes

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ const MEMORY_SIZE: usize = 4096;
 const STARTING_ADDR: u16 = 0x0200;
 
 fn main() {
-    let filename = "astrododge.ch8";
+    let filename = "pong.ch8";
 
     let contents = fs::read(filename).expect("Something went wrong reading the file");
 
@@ -91,9 +91,9 @@ enum Operation {
     AddConst { x: u8, byte: u8 },
     MovReg { x: u8, y: u8 },
     _Or { x: u8, y: u8 },
-    _AndNoCarry { x: u8, y: u8 },
+    _And { x: u8, y: u8 },
     _Xor { x: u8, y: u8 },
-    _AndCarry { x: u8, y: u8 },
+    _AddReg { x: u8, y: u8 },
     _Sub1Borrow { x: u8, y: u8 }, // set vf to 1 if borrows Vx = Vx - Vy
     _ShiftRight { x: u8 },        //bit 0 -> Vf
     _Sub2Borrow { x: u8, y: u8 }, // set vf to 1 if borrows Vx = Vy - Vx
@@ -101,7 +101,7 @@ enum Operation {
     _SkneReg { x: u8, y: u8 },
     _MovI { value: u16 },
     _JmpI { addr: u16 },
-    _Rand { x: u8, max: u16 },
+    _Rand { x: u8, max: u8 },
     _Sprite { x: u8, y: u8, s: u8 },
     _XSprite { x: u8, y: u8 },
     _SkKeyPress { key: u8 },
@@ -132,20 +132,20 @@ impl Operation {
             (0x00, 0xFE) => Operation::Low,
             (0x00, 0xFF) => Operation::High,
             (0x10...0x1F, _) => Operation::Jmp {
-                addr: (((instruction.0.clone() as u16) << 8) | instruction.1.clone() as u16)
+                addr: (((instruction.0.clone() as u16) << 8) | instruction.1 as u16)
                     & 0x0FFF,
             },
             (0x20...0x2F, _) => Operation::Jsr {
-                addr: (((instruction.0.clone() as u16) << 8) | instruction.1.clone() as u16)
+                addr: (((instruction.0 as u16) << 8) | instruction.1 as u16)
                     & 0x0FFF,
             },
             (0x30...0x3F, _) => Operation::SkeqConst {
                 x: instruction.0 & 0x0F,
-                byte: instruction.1.clone(),
+                byte: instruction.1,
             },
             (0x40...0x4F, _) => Operation::SkneConst {
                 x: instruction.0 & 0x0F,
-                byte: instruction.1.clone(),
+                byte: instruction.1,
             },
             (0x50...0x5F, _) => Operation::SkeqReg {
                 x: instruction.0 & 0x0F,
@@ -153,15 +153,61 @@ impl Operation {
             },
             (0x60...0x6F, _) => Operation::MovConst {
                 x: instruction.0 & 0x0F,
-                byte: instruction.1.clone(),
+                byte: instruction.1,
             },
             (0x70...0x7F, _) => Operation::AddConst {
                 x: instruction.0 & 0x0F,
-                byte: instruction.1.clone(),
+                byte: instruction.1,
             },
             (0x80...0x8F, _) if instruction.1 & 0x0F == 0x00 => Operation::MovReg {
                 x: instruction.0 & 0x0F,
                 y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, _) if instruction.1 & 0x0F == 0x01 => Operation::_And {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, _) if instruction.1 & 0x0F == 0x02 => Operation::_Or {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, _) if instruction.1 & 0x0F == 0x03 => Operation::_Xor {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, _) if instruction.1 & 0x0F == 0x04 => Operation::_AddReg {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, _) if instruction.1 & 0x0F == 0x05 => Operation::_Sub1Borrow {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, 0x06) => Operation::_ShiftRight {
+                x: instruction.0 & 0x0F,
+            },
+            (0x80...0x8F, _) if instruction.1 & 0x0F == 0x00 => Operation::_Sub2Borrow {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0x80...0x8F, 0x0E) => Operation::_ShiftLeft {
+                x: instruction.0 & 0x0F,
+            },
+            (0x90...0x9F, _) if instruction.1 & 0x0F == 0x00 => Operation::_SkneReg {
+                x: instruction.0 & 0x0F,
+                y: instruction.1 >> 4,
+            },
+            (0xA0...0xAF, _) => Operation::_MovI {
+                value: (((instruction.0 as u16) << 8) | instruction.1 as u16)
+                    & 0x0FFF,
+            },
+            (0xB0...0xBF, _) => Operation::_JmpI {
+                addr: (((instruction.0 as u16) << 8) | instruction.1 as u16)
+                    & 0x0FFF,
+            },
+            (0xC0...0xCF, _) => Operation::_Rand {
+                x: instruction.0 & 0x0F,
+                max:  instruction.1 ,
             },
             _ => Operation::NONE,
         }
@@ -174,13 +220,55 @@ impl Operation {
             }
             Operation::Jsr { addr } => {
                 resources.stack.push(resources.pc);
-                resources.pc = addr;
+                resources.pc = addr ;
             }
             Operation::SkeqConst { x, byte } => {
-                //                if
+                if resources.reg[x as usize] == byte {
+                    resources.pc += 1;
+                }
             }
-            Operation::MovConst { x , byte: b } => resources.reg[x as usize] = b,
+            Operation::SkneConst { x, byte } => {
+                if resources.reg[x as usize] != byte {
+                    resources.pc += 1;
+                }
+            }
+            Operation::SkeqReg { x, y } => {
+                if resources.reg[x as usize] == resources.reg[y as usize] {
+                    resources.pc += 1;
+                }
+            }
+            Operation::MovConst { x, byte: b } => resources.reg[x as usize] = b,
             Operation::AddConst { x, byte: b } => resources.reg[x as usize] += b,
+            Operation::MovReg {x, y}=> resources.reg[x as usize] = resources.reg[y as usize],
+            Operation::_Or {x, y}=>  resources.reg[x as usize] |= resources.reg[y as usize],
+            Operation::_And {x, y} =>  resources.reg[x as usize] &= resources.reg[y as usize],
+            Operation::_Xor {x,y} =>  resources.reg[x as usize] ^= resources.reg[y as usize],
+            Operation::_AddReg { x, y } => {
+                resources.reg[x as usize] += resources.reg[y as usize];
+                if resources.reg[x as usize] < x { resources.reg[0xf] = 1; }
+            },
+            Operation::_Sub1Borrow { x, y } => {
+                if resources.reg[x as usize] > resources.reg[y as usize] { resources.reg[0xf] = 1; }
+                resources.reg[x as usize] = resources.reg[x as usize] - resources.reg[y as usize];
+            },
+            Operation::_ShiftRight { x } => {
+                resources.reg[0xf] = resources.reg[x as usize] & 0b0000_0001;
+                resources.reg[x as usize] = resources.reg[x as usize] >> 1;
+            },
+            Operation::_Sub2Borrow { x, y } => {
+                if resources.reg[y as usize] > resources.reg[x as usize] { resources.reg[0xf] = 1; }
+                resources.reg[x as usize] = resources.reg[y as usize] - resources.reg[x as usize];
+            },
+            Operation::_ShiftLeft { x } => {
+                resources.reg[0xf] = resources.reg[x as usize] & 0b1000_0000;
+                resources.reg[x as usize] = resources.reg[x as usize] << 1;
+            },
+            Operation::_SkneReg { x, y } => {
+                if resources.reg[x as usize] != resources.reg[y as usize] {
+                    resources.pc += 1;
+                }
+            },
+            Operation::_MovI { value } => resources.reg_i = value,
             _ => (),
         }
     }
